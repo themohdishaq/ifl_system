2fbe4f001b6839ccdb930c4b3210d88989d7ed86
Abubakar <abubakarimran553@gmail.com>
2024-03-31 15:31:50 +0500

    messaging page updated3

--- src/components/DashBoard.jsx
+++ src/components/DashBoard.jsx
@@ -6,7 +6,7 @@ import PieGraph from "./PieGraph";
 export default function DashBoard() {
   return (
     <div className="relative w-full md:h-screen bg-black">
-      <div class="grid md:grid-row-7 h-full">
+      <div class="grid md:grid-row-7 h-full w-full">
         <div className="m-3 grid md:grid-cols-4 grid-row-4 gap-4">
           <div className="bg-green-400 rounded p-3">Earnings</div>
           <div className="bg-green-400 rounded p-3">Students</div>
