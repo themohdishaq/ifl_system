5d4792a36cf1a0f4bb24f87d2cf5cb48481f6d13
MSS <sarmadsaleem.333@gmail.com>
2024-04-08 23:37:00 +0500

    Modified modals

--- backend/Models/Transaction.js
+++ backend/Models/Transaction.js
@@ -1,19 +1,23 @@
-const moongose = require('mongoose');
+const moongose = require("mongoose");
 const { Schema } = moongose;
 const TransactionSchema = new Schema({
-    donor: {
-        type: moongose.Schema.Types.ObjectId,
-        required: true
-    },
-    approved_case: {
-        type: moongose.Schema.Types.ObjectId,
-        required: true
-    },
-    payment_no: {
-        type: Number,
-        required: true
-    }
+  donor: {
+    type: moongose.Schema.Types.ObjectId,
+    required: true,
+  },
+  approved_case: {
+    type: moongose.Schema.Types.ObjectId,
+    required: true,
+  },
+  payment_no: {
+    type: Number,
+    required: true,
+  },
+  date: {
+    type: Date,
+    default: Date.now,
+  },
 });
 
-const Transaction = moongose.model('transactions', TransactionSchema);
+const Transaction = moongose.model("transactions", TransactionSchema);
 module.exports = Transaction;
