12c7c7a2936a5f8bd821719c5e3e71bc904145b6
Abubakar <abubakarimran553@gmail.com>
2024-04-08 18:07:58 +0500

    database models created.

---
+++ backend/Models/ApprovedCase.js
@@ -0,0 +1,31 @@
+const moongose = require('mongoose');
+const { Schema } = moongose;
+const ApprovedCaseSchema = new Schema({
+    admin: {
+        type: moongose.Schema.Types.ObjectId,
+        required: true
+    },
+    request: {
+        type: moongose.Schema.Types.ObjectId,
+        required: true
+    },
+    startDate: {
+        type: Date,
+        required: true
+    },
+    endDate: {
+        type: Date,
+        required: true
+    },
+    postedDate: {
+        type: Date,
+        required: true
+    },
+    payments_completed: {
+        type: Number,
+        required: true
+    }
+});
+
+const ApprovedCase = moongose.model('approved_case', ApprovedCaseSchema);
+module.exports = ApprovedCase;
---
+++ backend/Models/Transaction.js
@@ -0,0 +1,19 @@
+const moongose = require('mongoose');
+const { Schema } = moongose;
+const TransactionSchema = new Schema({
+    donor: {
+        type: moongose.Schema.Types.ObjectId,
+        required: true
+    },
+    approved_case: {
+        type: moongose.Schema.Types.ObjectId,
+        required: true
+    },
+    payment_no: {
+        type: Number,
+        required: true
+    }
+});
+
+const Transaction = moongose.model('transactions', TransactionSchema);
+module.exports = Transaction;
