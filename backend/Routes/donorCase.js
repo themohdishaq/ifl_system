8fade9274f78d0bc76e73fdc0a5b1f42d35cbe1b
MSS <sarmadsaleem.333@gmail.com>
2024-04-09 02:00:55 +0500

    added sponser case by admin

--- backend/Routes/donorCase.js
+++ backend/Routes/donorCase.js
@@ -7,16 +7,37 @@ const Admin = require("../Models/Admin");
 const Donor = require("../Models/Donor");
 const ApprovedCase = require("../Models/ApprovedCase");
 
-//show all verified cases to donor which are approved by admin
+//show all verified cases to donor which are approved by admin whose date has been started but not ended
+
+// !!!!!!  rememeber here i did not remove the feature where if case is under  a specified dnor still that case is visibkle
+//!!!! will remove that thing soon
+
 router.get(
   "/get_all_approved_cases_by_donor",
   fetchDonor,
-  [
-    body("startDate", "Enterrhe start date of the case"),
-    body("endDate", "Enter the end date of the case"),
-    body("payment_timeline", "Enter the payment timeline"),
-    body("total_payments", "Enter the total payments"),
-  ],
+
+  async (req, res) => {
+    const currentDate = new Date();
+    try {
+      const approved_cases = await ApprovedCase.find({
+        endDate: { $gt: currentDate },
+      }).populate("request");
+      if (!approved_cases) {
+        return res.json("No approved cases found");
+      }
+      res.json(approved_cases);
+    } catch (error) {
+      res.json("Error fetching approved cases");
+    }
+  }
+);
+
+//route to sponser case by donor
+
+router.post(
+  "/sponser_case_by_donor/:id",
+  fetchDonor,
+  [body("commited_payments", "Enter the commited payments")],
   async (req, res) => {
     const errors = validationResult(req);
     if (!errors.isEmpty()) {
@@ -24,46 +45,49 @@ router.get(
       console.log(response[0].msg);
       return res.status(400).json(response[0].msg);
     }
-    const req_admin = await Admin.findById(req.user.id);
-    const { startDate, endDate, payment_timeline, total_payments } = req.body;
     try {
-      let approved_case = await ApprovedCase.create({
-        admin: req.user.id,
-        request: req.params.id,
-        startDate: startDate,
-        endDate: endDate,
-        payment_timeline: payment_timeline,
-        total_payments: total_payments,
-        payments_completed: 0,
+      const { commited_payments } = req.body;
+      const req_case = await ApprovedCase.findById(req.params.id)
+        .populate("request")
+        .populate("student");
+      let checkAlreadyCases = await WorksOn.findOne({
+        approved_case: req.params.id,
       });
-      if (!approved_case) {
-        return res.json("Error sending request");
+      if (checkAlreadyCases) {
+        return res.json("This case has already been sponsored by someone");
       }
-      await Request.findByIdAndUpdate(req.params.id, {
-        status: "approved",
+      if (
+        req_case.payments_completed + commited_payments >
+        req_case.total_payments
+      ) {
+        return res.json("You are trying to pay more than the total payments");
+      }
+      let sponsered_case = await WorksOn.create({
+        donor: req.user.id,
+        approved_case: req.params.id,
+        commited_payments: commited_payments,
+        completed_payments: 0,
       });
-
-      //sending notification to student and admin
-      let request_by_student = await Request.findById(req.params.id);
+      let donor = await Donor.findById(req.user.id);
       await Notifications.create({
         user: req.user.id,
-        type: "admin",
-        message:
-          "Case has been approved successfully with description " +
-          request_by_student.description,
+        type: "donor",
+        message: "You Case has been sponsered successfully ",
       });
       await Notifications.create({
-        user: request_by_student.student,
+        user: req_case.request.student,
         type: "student",
-        message:
-          "Your Case has been approved successfully with description " +
-          request_by_student.description,
+        message: "Case has been sponsered successfully by " + donor.name,
       });
-      res.json("Case  been approved successfully");
-    } catch (error) {
-      res.json("Error sending request");
+      await Notifications.create({
+        user: req_case.admin,
+        type: "admin",
+        message: "Case has been sponsered successfully by " + donor.name,
+      });
+      return res.json("Case has been sponsered successfully");
+    } catch {
+      return res.json("Error sponsering case");
     }
   }
 );
-
 module.exports = router;
