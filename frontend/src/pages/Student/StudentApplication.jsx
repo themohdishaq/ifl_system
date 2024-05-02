7d35b1a07593b861a3511b887f1ede6bf776bb4f
Abubakar <abubakarimran553@gmail.com>
2024-05-02 21:59:49 +0500

    image issue

--- frontend/src/pages/Student/StudentApplication.jsx
+++ frontend/src/pages/Student/StudentApplication.jsx
@@ -1,82 +1,179 @@
-import React, { useState } from 'react';
+import React, { useRef, useState } from 'react';
 import { Button, TextField, Typography, Link, Grid, Paper } from '@mui/material';
+import { Form, Formik } from 'formik';
+import { studentApplicationSchema } from '../../utils/Schemas';
+import { useMutation } from '@tanstack/react-query';
+import axios from 'axios';
 
 export default function StudentApplication() {
-    const [selectedImage, setSelectedImage] = useState(null);
+    const closeRef = useRef(null);
+    const [studentApplicationImage, setStudentApplicationImage] = useState({ image: "" });
+    const onImageChange = (e) => {
+        if (e.target.files.length > 0) {
+            setStudentApplicationImage(e.target.files[0]);
+        }
+    };
+    const studentApplicationMutation = useMutation({
+        mutationFn: async (values) => {
+            const url = `http://localhost:3333/ifl_system/student/request_by_student`;
 
-    const handleImageChange = (event) => {
-        const file = event.target.files[0];
-        setSelectedImage(file);
-    }
+            // Create a new FormData instance
+            const formData = new FormData();
 
-  return (
-    <div>
-      <Grid container spacing={3} className="p-5 md:p-8">
-            <Grid item xs={12}>
-                <Typography variant="h4">User Profile</Typography>
-            </Grid>
-            <Grid item xs={12}>
-                <Link href="/">Home </Link>
-                <Typography variant="body">/ </Typography>
-                <Link href="/messaging"> User Profile</Link>
-            </Grid>
-            <Grid item xs={12} lg={4}>
-                <Paper className="flex flex-col justify-center items-center p-1 md:p-5">
-                    <div className="m-5 rounded-full ">
-                        <input
-                            type="file"
-                            accept="image/*"
-                            id="imageInput"
-                            style={{ display: 'none' }}
-                        />
-                        <label htmlFor="imageInput">
-                            <img src={selectedImage ? URL.createObjectURL(selectedImage) : 'https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg'} alt="" width='150px' height='150px' />
-                        </label>
-                    </div>
-                    <div className='flex justify-center items-center'>
-                        <Button
-                            variant="contained"
-                            component="span"
-                            onClick={handleImageChange}
-                            sx={{ bgcolor: '#1E3A8A', '&:hover': { bgcolor: '#1C2D61' }, mt: 2 }}
-                        >
-                            Upload
-                        </Button>
-                    </div>
-                </Paper>
-            </Grid>
-            <Grid item xs={12} lg={8}>
-                <Grid container spacing={3}>
-                    <Grid item xs={12} lg={12}>
-                        <Paper className="rounded-lg">
+            // Append the form values and the image file to the FormData instance
+            Object.keys(values).forEach(key => formData.append(key, values[key]));
+            formData.append('image', studentApplicationImage);
+            console.log(formData.get('image'));
+            console.log(formData.get('title'));
+            console.log(formData.get('description'));
 
-                            <Grid container xs={12} lg={12}>
-                                <Grid item xs={12} lg={12}>
-                                    <form className="w-full flex flex-col justify-around px-5 lg:p-5">
-                                        <TextField label="First Name" variant="outlined" size="small" sx={{ mb: 2 }} />
-                                        <TextField label="Phone Number" variant="outlined" size="small" sx={{ mb: 2 }} />
-                                    </form>
-                                </Grid>
+
+            // Send the FormData instance in the POST request
+            return axios.post(url, formData);
+        },
+        onError: (error) => {
+            console.log(error);
+            toast.error("Error sending request...");
+        },
+        onSuccess: (data) => {
+            if (data.data.error) {
+                toast.error(data.data.error);
+            } else {
+                toast.success(data.data);
+                closeRef.current.click();   
+            }
+        },
+    });
+
+    // const createFormData=(values)=>{
+    //     console.log(values.title)
+    //     const formData = new FormData();
+    //     formData.append("image", studentApplicationImage.image);
+    //     formData.append("title", values.title);
+    //     formData.append("description", values.description);
+    //     studentApplicationMutation.mutate(formData);
+    // }
+
+    return (
+        <div>
+            <Formik
+                initialValues={{
+                    title: "",
+                    description: ""
+                }}
+                validationSchema={studentApplicationSchema}
+                onSubmit={studentApplicationMutation.mutate}
+            >
+                {({
+                    errors,
+                    touched,
+                    values,
+                    handleChange,
+                    handleBlur,
+                    handleSubmit,
+                }) => (
+                    <Form>
+                        <Grid container spacing={3} className="p-5 md:p-8">
+                            <Grid item xs={12}>
+                                <Typography variant="h4">User Profile</Typography>
                             </Grid>
-
-                            <Grid item xs={12} lg={12}>
-                                <div className='w-full flex justify-center'>
-                                    <div className="mb-3">
+                            <Grid item xs={12}>
+                                <Link href="/">Home </Link>
+                                <Typography variant="body">/ </Typography>
+                                <Link href="/messaging"> User Profile</Link>
+                            </Grid>
+                            <Grid item xs={12} lg={4}>
+                                <Paper className="flex flex-col justify-center items-center p-1 md:p-5">
+                                    <div className="m-5 rounded-full ">
+                                        {/* <input
+                                            type="file"
+                                            accept="image/*"
+                                            id="imageInput"
+                                            style={{ display: 'none' }}
+                                            onChange={onImageChange}
+                                        /> */}
+                                        {/* <label htmlFor="imageInput">
+                                            <img src={selectedImage ? URL.createObjectURL(selectedImage) : 'https://static.vecteezy.com/system/resources/thumbnails/002/318/271/small/user-profile-icon-free-vector.jpg'} alt="" width='150px' height='150px' />
+                                        </label> */}
+                                        <i className="fa-regular fa-images mx-2 my-2" ></i> 
+                                         <input type="file" accept='image/*' onChange={onImageChange} /> 
+                                    </div>
+                                    {/* <div className='flex justify-center items-center'>
                                         <Button
                                             variant="contained"
-                                            sx={{ bgcolor: '#1E3A8A', '&:hover': { bgcolor: '#1C2D61' } }}
+                                            component="span"
+                                            sx={{ bgcolor: '#1E3A8A', '&:hover': { bgcolor: '#1C2D61' }, mt: 2 }}
                                         >
-                                            Submit
+                                            Upload
                                         </Button>
-                                    </div>
-                                </div>
+                                    </div> */}
+                                </Paper>
                             </Grid>
+                            <Grid item xs={12} lg={8}>
+                                <Grid container spacing={3}>
+                                    <Grid item xs={12} lg={12}>
+                                        <Paper className="rounded-lg">
 
-                        </Paper>
-                    </Grid>
-                </Grid>
-            </Grid>
-        </Grid>
-    </div>
-  )
+                                            <Grid container>
+                                                <Grid item xs={12} lg={12}>
+                                                    <form className="w-full flex flex-col justify-around px-5 lg:p-5">
+                                                        <TextField
+                                                            required
+                                                            fullWidth
+                                                            id="titel"
+                                                            label="Title"
+                                                            name="title"
+                                                            sx={{ mb: 3 }}
+                                                            onChange={handleChange}
+                                                            onBlur={handleBlur}
+                                                            value={values.title}
+                                                            error={errors.title && touched.title}
+                                                            helperText={errors.title && touched.title && errors.title}
+                                                            autoComplete="title"
+                                                            autoFocus
+                                                        />
+                                                        <TextField
+                                                            required
+                                                            fullWidth
+                                                            id="description"
+                                                            label="Description"
+                                                            name="description"
+                                                            sx={{ mb: 3 }}
+                                                            onChange={handleChange}
+                                                            onBlur={handleBlur}
+                                                            value={values.description}
+                                                            error={errors.description && touched.description}
+                                                            helperText={errors.description && touched.description && errors.description}
+                                                            autoComplete="description"
+                                                            autoFocus
+                                                        />
+                                                    </form>
+                                                </Grid>
+                                            </Grid>
+
+                                            <Grid item xs={12} lg={12}>
+                                                <div className='w-full flex justify-center'>
+                                                    <div className="mb-3">
+                                                        <Button
+                                                            type="submit"
+                                                            fullWidth
+                                                            variant="contained"
+                                                            sx={{ mt: 3, mb: 2 }}
+                                                            onClick={handleSubmit}
+                                                        >
+                                                            Submit
+                                                        </Button>
+                                                    </div>
+                                                </div>
+                                            </Grid>
+                                        </Paper>
+                                    </Grid>
+                                </Grid>
+                            </Grid>
+                        </Grid>
+                    </Form>
+                )}
+            </Formik>
+        </div>
+    )
 }
