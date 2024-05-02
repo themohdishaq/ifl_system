d32b1b2ba2564b073f6ad56729425ea416a28a94
MSS <sarmadsaleem.333@gmail.com>
2024-05-02 20:37:23 +0500

    Fixed the bug

--- backend/Routes/studentCase.js
+++ backend/Routes/studentCase.js
@@ -59,19 +59,88 @@ router.post(
   }
 );
 
-router.get("/student/case_applications", fetchStudent, async (req, res) => {
+// get all request of student
+router.get(
+  "/student/get_all_requests_by_student",
+  fetchStudent,
+  async (req, res) => {
+    try {
+      const requests = await Request.find({ student: req.user.id });
+      res.json(requests);
+    } catch (error) {
+      console.log(error);
+      return res.json("Error fetching requests");
+    }
+  }
+);
+
+// get all current approved cases of student
+router.get("/student/approved-cases", fetchStudent, async (req, res) => {
+  const currentDate = new Date();
   try {
-    const student = await Student.findById(req.user.id);
-    if (!student) {
-      return res.json("Student not found");
+    const approved_cases = await ApprovedCase.find({
+      student: req.user.id,
+      endDate: { $gt: currentDate },
+    }).populate("request");
+    if (!approved_cases) {
+      return res.json("No approved cases found");
     }
-    const case_applications = await Request.find({ student: req.user.id });
-    if (!case_applications) {
-      return res.json("No case applications found");
-    }
-    res.json(case_applications);
+    res.json(approved_cases);
   } catch (error) {
-    res.json("Error fetching case applications");
+    res.json("Error fetching approved cases");
+  }
+});
+
+// route to get complted approbved cses by student
+router.get("/student/completed-cases", fetchStudent, async (req, res) => {
+  const currentDate = new Date();
+  try {
+    const approved_cases = await ApprovedCase.find({
+      student: req.user.id,
+      endDate: { $lt: currentDate },
+    }).populate("request");
+    if (!approved_cases) {
+      return res.json("No approved cases found");
+    }
+    res.json(approved_cases);
+  } catch (error) {
+    res.json("Error fetching approved cases");
+  }
+});
+
+// route to get all notifications of student
+router.get("/student/notifications", fetchStudent, async (req, res) => {
+  try {
+    const notifications = await Notifications.find({ user: req.user.id });
+    res.json(notifications);
+  } catch (error) {
+    console.log(error);
+    res.json("Error fetching notifications");
+  }
+});
+
+router.get("/student/view-application/:id", fetchStudent, async (req, res) => {
+  try {
+    const request = await Request.findById(req.params.id);
+    if (!request) {
+      return res.json("No application found");
+    }
+    res.json(request);
+  } catch (error) {
+    console.log(error);
+    res.json("Error fetching application");
+  }
+});
+
+router.get("/student/view-case/:id", async (req, res) => {
+  try {
+    let approvedCase = await ApprovedCase.findById(req.params.id).populate(
+      "request"
+    );
+    res.json(approvedCase);
+  } catch (error) {
+    console.log(error);
+    return res.json("Error fetching requests");
   }
 });
 module.exports = router;
